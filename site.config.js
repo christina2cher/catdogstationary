ca8086aef306cf6f4c9ef136f695c2fa08ce0c58
christina2cher <christina2cher@outlook.com>
2024-06-25 09:10:53 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "christina2cher/catdogstationary",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
